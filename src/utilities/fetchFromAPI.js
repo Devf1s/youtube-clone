361dbcd02834c39efbb8209e928dcbef4f760b1e
Kirito <dragongames829@gmail.com>
2023-06-11 22:17:11 +0100

    update RapidAPI call

--- src/utilities/fetchFromAPI.js
+++ src/utilities/fetchFromAPI.js
@@ -1,33 +1,18 @@
-// import axios from 'axios';
-
-// const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
-
-// const options = {
-// 	params: {
-// 		maxResults: '50'
-// 	},
-// 	headers: {
-// 		'Access-Control-Allow-Credentials':true,
-// 		'X-RapidAPI-Key': '91766d6993msh3bd85c80e2cd0a0p118319jsn781536a29bad',
-// 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-// 	}
-// };	
-
-// export const fetchFromAPI = async (url) => {
-// 	const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-// 	return data;
-// };
-
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube.googleapis.com/youtube/v3';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
 	params: {
-		key: 'AIzaSyDt5UiC0GjBFC8rbn7ZTMM7cc06hl0l_Fg',
 		maxResults: '50'
+	},
+	headers: {
+		'Access-Control-Allow-Credentials': true,
+		'X-RapidAPI-Key': 'c02d2380ddmsh7b9e4a1a18fff95p1bfdedjsn4eb67ac04abc',
+		'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
 	}
 };
+
 export const fetchFromAPI = async (url) => {
 	const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 	return data;
